b280648f1b64773fe931a1a76605bed09e09dd1d
Wellington Mitrut <wellingtonmitrut@gmail.com>
2020-06-06 14:39:23 -0300

    forgot the main file

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var customerly = require('./src/react-customerly');
 
 module.exports = {
 	customerly: {    
-        initialize: function initialize(app_id, mWidget, attachments, screenshot, lang) {
-            customerly(app_id, mWidget, attachments, screenshot, lang);
+        initialize: function initialize(app_id, mWidget, attachments, screenshot, lang, visible, color, position) {
+            customerly(app_id, mWidget, attachments, screenshot, lang, visible, color, position);
         }
 	}
 };
