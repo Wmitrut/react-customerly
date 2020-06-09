0b679fa2b094ade0c6bb71fdc1d3aec86146055e
Emma FABRE <ehtnam6@gmail.com>
2020-06-09 16:37:41 +0200

    Use object as options

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
-var customerly = require('./src/react-customerly');
-
 module.exports = {
-	customerly: {    
-        initialize: function initialize(app_id, mWidget, attachments, screenshot, lang, visible, color, position) {
-            customerly(app_id, mWidget, attachments, screenshot, lang, visible, color, position);
-        }
-	}
+    customerly: {
+        initialize: function (appId, options) {
+            window.customerlySettings = {
+                app_id: appId,
+                ...options,
+            };
+
+            function loadCustomerly() {
+                let e = t.createElement("script");
+                (e.type = "text/javascript"),
+                    (e.async = !0),
+                    (e.src = "https://widget.customerly.io/widget/" + appId);
+                let r = t.getElementsByTagName("script")[0];
+                r.parentNode.insertBefore(e, r);
+            }
+
+            let r = window,
+                t = document,
+                n = function () {
+                    n.c(arguments);
+                };
+
+            (r.customerly_queue = []),
+                (n.c = function (e) {
+                    r.customerly_queue.push(e);
+                }),
+                (r.customerly = n),
+                r.attachEvent
+                    ? r.attachEvent("onload", loadCustomerly)
+                    : r.addEventListener("load", loadCustomerly, !1);
+        },
+    },
 };
